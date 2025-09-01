ae07f81a45dfa604e3f9831138be4f8d7cd6dbe1
hoyitsowen <owen.trinidad23@gmail.com>
2025-09-01 15:02:01 +0800

    Realignment on Routing path

--- src/App.js
+++ src/App.js
@@ -6,21 +6,17 @@ import RegisterPage from './Visitor-side/RegisterPage';
 import ForgotPassword from './Visitor-side/ForgotPassword';
 import OtpCodePage from "./Visitor-side/OtpCodePage";
 import ResetPassword from "./Visitor-side/ResetPassword";
-import AdminLogin from "./Admin-side/AdminLogin";
 import Error from "./Visitor-side/Error";
 import AssessmentWelcome from "./User-side/AssessmentWelcome";
 import UserHomepage from "./User-side/UserHomepage";
 import UserResults from "./User-side/UserResults";
 import UserResultsHistory from "./User-side/UserResultsHistory";
 import ProfilePage from "./User-side/ProfilePage";
-<<<<<<< HEAD
-import AdminDashboard from './Admin-side/AdminDashboard';
-=======
 import NoResultPage from "./User-side/NoResultPage";
 import AssessmentPage from "./User-side/AssessmentPage";
+import AdminDashboard from './Admin-side/AdminDashboard';
+import AdminLogin from "./Admin-side/AdminLogin";
 
-
->>>>>>> 125946bdf4c8522b9fed951fc19e4fea1b2efb8e
 
 
 function App() {
@@ -40,14 +36,11 @@ function App() {
           <Route path="/results" element={<UserResults />} />
           <Route path="/results-history" element={<UserResultsHistory />} />
           <Route path="/profile" element={<ProfilePage />} />
-<<<<<<< HEAD
-          <Route path="/admin-login" element={<AdminLogin />} />
-          <Route path="/admin-dashboard" element={<AdminDashboard />} />
-=======
           <Route path="/noresult" element={<NoResultPage />} />
           <Route path="/assessment-test" element={<AssessmentPage />} />
+          <Route path="/admin" element={<AdminLogin />} />
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
 
->>>>>>> 125946bdf4c8522b9fed951fc19e4fea1b2efb8e
         </Routes>
       </div>
     </Router>
